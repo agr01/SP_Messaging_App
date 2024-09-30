c54e333c158814bc1fbd12b320495d30fc18d527
Alex <a.t.gramss@gmail.com>
2024-09-30 21:44:56 +0930

    New dark theme

--- messaging-client/tailwind.config.js
+++ messaging-client/tailwind.config.js
@@ -4,16 +4,16 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "dark-1":"#111620", // darkest
-        "dark-2":"#161C29",
-        "dark-3":"#151b2a", 
-        "dark-4":"#1a202f", 
-        "dark-5":"#546694", // lightest
+        "dark-1":"#121212", // darkest
+        "dark-2":"#161616",
+        "dark-3":"#1e1e1e",
+        "dark-4":"#272727", 
+        "dark-5":"#303030",  // lightest
 
         "light-1":"#b0bfe6", //abbeed
         "light-2":"#6c7da7",
 
-        "accent-1":"#e9d5ff",
+        "accent-1":"hsl(180, 60%, 70%)",
         "accent-2":"#d8b4fe"
 
       },
@@ -23,3 +23,9 @@ module.exports = {
   plugins: [],
 }
 
+// Blue background colors
+// "dark-1":"#111620", // darkest
+// "dark-2":"#161C29",
+// "dark-3":"#151b2a", 
+// "dark-4":"#1a202f", 
+// "dark-5":"#546694", // lightest
